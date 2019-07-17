c808d8455536d5f02b88ba9532263927f37cdc40
Ubuntu <ubuntu@ip-172-31-92-130.ec2.internal>
2019-07-17 00:26:20 +0000

    setup Heroku addon for PostgreSQL database in Django

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse('postgres://obaumppixnrkek:267732d36dc01ef5a023680ab3a0f5b57b1ea2f01f8cc52c991f1669fea24b8f@ec2-54-228-246-214.eu-west-1.compute.amazonaws.com:5432/d3f6e3ljcf76ge')}
 
 
 # Password validation
